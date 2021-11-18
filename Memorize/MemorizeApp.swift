d7f3a9224c76fe8d846afae1f2e2fe1538708075
Frazile Inc <Frazile.com@gmail.com>
2021-11-18 10:47:48 +0530

    Memorize

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Parth Aggarwal on 18/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
